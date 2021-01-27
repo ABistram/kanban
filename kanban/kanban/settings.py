cef17979a9f475606704adc4765d46aa3225d289
ABistram <78019133+ABistram@users.noreply.github.com>
2021-01-27 13:10:18 +0100

    V.0.1.1(users)
    User manager branch

--- kanban/kanban/settings.py
+++ kanban/kanban/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users_manager.apps.UsersManagerConfig'
     'table.apps.TableConfig',
     'django.contrib.admin',
     'django.contrib.auth',
