996367fb7e90b60f49d1c5eabf9cef7a42f053f0
ABistram <78019133+ABistram@users.noreply.github.com>
2021-01-27 10:34:01 +0100

    V.0.1.1
    Added views in table models

--- kanban/table/models.py
+++ kanban/table/models.py
@@ -1,6 +1,71 @@
 from django.db import models
 
 # Create your models here.
+"""
+Table
+-id(PK)
+-title
+-sizeX
+-sizeY
+-type(private/public)
+
+Field
+-id(PK)
+-name
+-type
+-label
+-positionX
+-positionY
+-rowspan(def 1)
+-colspan(def 1)
+-table(FK to Table)
+
+Item
+-id(PK)
+-type
+-content
+-field(FK to Field)
+"""
+
+
+class Table(models.Model):
+    title = models.CharField(max_length=100)
+    sizeX = models.IntegerField()
+    sizeY = models.IntegerField()
+    PRIVATE = "PV"
+    PUBLIC = "PB"
+    privacy_type_choices = [
+        (PRIVATE, "Private"),
+        (PUBLIC, "Public")
+    ]
+    privacy_type = models.CharField(
+        max_length=32,
+        choices=privacy_type_choices,
+        default=PRIVATE
+    )
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=32)
+    MIXED = "MX"
+    LABEL = "LB"
+    STICKER = "ST"
+    field_type_choices = [
+        (LABEL, "Label"),
+        (STICKER, "Sticker"),
+        (MIXED, "Mixed")
+    ]
+    field_type = models.CharField(
+        max_length=32,
+        choices=field_type_choices,
+        default=LABEL
+    )
+    label = models.CharField(max_length=256)
+    positionX = models.IntegerField()
+    positionY = models.IntegerField()
+    rowspan = models.IntegerField(default=1)
+    colspan = models.IntegerField(default=1)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
 
 
 class Item(models.Model):
@@ -18,7 +83,5 @@ class Item(models.Model):
         default=EPICS
     )
     content = models.TextField(default="")
-
-
-class Table(models.Model):
-    title = models.CharField(max_length=100)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    
